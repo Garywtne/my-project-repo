9ce313d6839e9d88113dbfb668b54804b25978da
Garywtne <gary.whitney@stiga.com>
2022-04-18 16:39:39 +0100

    edit config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-
-# OpenWeatherMap API Key
-weather_api_key = "36426fe7b8f897a2610ab86f3f9f9741"
-
-# Google API Key
-gkey = "AIzaSyCAx9ERve1QU0XokdNREgmLjKkBdtr36No"
-
-# Census API Key
-api_key = "562507981f10535c9c30e70c4e790e64950601eb"
